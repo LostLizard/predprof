6974b4ce4c73e9eb46f7ab5acf29d64721aeafdd
Vlad <vsakayafignavlad@gmail.com>
2021-03-21 16:40:38 +0300

    форт

---
+++ src/main/resources/templates/mainscript.js
@@ -0,0 +1,49 @@
+var cityData;
+var mediansData;
+function getCityData(cityId){
+    let url="/tem?cityId="+cityId;
+    let x=new XMLHttpRequest();
+    x.open("GET",url,true);
+    x.send(null);
+    x.onload=function(){
+        if(x.status==200){
+            cityData=prognozData(parser(x.responseText));
+                 alert(cityData.length);
+                 alert(cityData);
+                 alert(cityData[364]);
+        }
+    }
+}
+function parser(data){
+    let help=data.split("#");
+    let secondData;
+    for(let i=0;i<help.length;i++){
+        secondData[i/20.0][i%20]=help[i];
+    }
+}
+function prognozData(data){
+    let helpData;
+    let nextData;
+    for(let i=1;i<data.length;i++){
+        for(let j=0;j<data[i].length;j++){
+            helpData[j]=helpData[j]+(data[i][j]-data[i-1][j])
+        }
+    }
+    for(let i=0;i<data[0].length;i++){
+        helpData[i]=helpData[i]/(data.length-1);
+        nextData[i]=data[data.length][i]+helpData[i];
+    }
+    return nextData;
+}
+function getMedians(cityId){
+    let url="/med?cityId="+cityId;
+    let x=new XMLHttpRequest();
+    x.open("GET",url,true);
+    x.send(null);
+    x.onload=function(){
+        if(x.status==200){
+           mediansData=parser(x.responseText);
+           alert(mediansData);
+        }
+    }
+}
