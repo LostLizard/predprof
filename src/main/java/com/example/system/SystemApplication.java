0165abe383bb7131b82e262c2a75f1bffaac00f6
Vlad <vsakayafignavlad@gmail.com>
2021-03-21 15:33:57 +0300

    work with data

--- src/main/java/com/example/system/SystemApplication.java
+++ src/main/java/com/example/system/SystemApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SystemApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(SystemApplication.class, args);
+
     }
 
 }
