e3994926194a63c15e0ade9331ff41bd35b4525c
nikita <nmkazantsev@gmail.com>
2021-03-21 11:21:36 +0300

    пепый гоород

--- src/main/java/com/example/system/controllers/MainController.java
+++ src/main/java/com/example/system/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.example.system.controllers;
 
 import com.example.system.controllers.services.TempService;
+import com.example.system.data.WorkWithData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
     @Autowired
@@ -16,21 +21,24 @@ public class MainController {
 
     @GetMapping("/")
     public String home() {
-        WorkWithData w = new WorkWithData();
-
+        ArrayList<Float> temps = WorkWithData.takeGoodList("/media/nikita/Samsung_T5/sharedFiles/system/src/main/java/com/example/system/data/Алмазный.txt");
+        for (int i = 0; i < temps.size(); i++) {
+            tempService.addTemp(temps.get(i), null, 1l);
+        }
         return "hello";
     }
+
     @GetMapping("/day")
     public ResponseEntity<String> day(@RequestParam(value = "city") String city) {
-       double [] answ= tempService.getAllTempsByDays(city);
-        String answer ="";
-        for(int i=0;i<answ.length;i++){
-            answer+=answ[i];
-            if(i!=answ.length-1) {
+        double[] answ = tempService.getAllTempsByDays(city);
+        String answer = "";
+        for (int i = 0; i < answ.length; i++) {
+            answer += answ[i];
+            if (i != answ.length - 1) {
                 answer += "#";
             }
         }
-       return  new ResponseEntity<String> (answer, HttpStatus.OK);
+        return new ResponseEntity<String>(answer, HttpStatus.OK);
     }
 
 
