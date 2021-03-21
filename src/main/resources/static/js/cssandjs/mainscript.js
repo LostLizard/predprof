94ce38b13057ed05c5912202cf5c67a9d4b8256c
nikita <nmkazantsev@gmail.com>
2021-03-21 14:42:59 +0300

    save

--- src/main/resources/static/js/cssandjs/mainscript.js
+++ src/main/resources/static/js/cssandjs/mainscript.js
@@ -1,6 +1,7 @@
 var cityData=new Array(365).fill(0);
 var mediansData;
-function getCityData(cityId){
+function getCityData(){
+    let cityId=document.getElementById("id").value;
     let url="/day?cityId="+cityId;
     let x=new XMLHttpRequest();
     x.open("GET",url,true);
@@ -9,6 +10,12 @@ function getCityData(cityId){
         if(x.status==200){
             let dayData=x.responseText.split("#");
             cityData=prognozData(dayData);
+            alert("Завтра "+Math.round(cityData[0]));
+            let str="";
+            for(let i=0;i<7;i++){
+                str=str+Math.round(cityData[i])+" ";
+            }
+            alert("Следующая неделя "+str);
          //   mediansData=getMedians(dayData);
         }
         else {
@@ -19,7 +26,7 @@ function getCityData(cityId){
 function prognozData(data){
     let helpData=new Array(365).fill(0.0);
     for(let i=0;i<365;i++){
-        helpData[i]=(data[365*19+i]-data[i])/19;//helpData[i]+
+        helpData[i]=(data[365*19+i]-data[i]);//19;//helpData[i]+
     }
     return helpData;
 }
